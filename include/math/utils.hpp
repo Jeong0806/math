b47bc05b7da10a228923822cccff76040bd38b6c
Jeong0806 <Jeong0806@github.com>
2023-12-23 03:10:53 +0900

    Updated skeleton codes

---
+++ include/math/utils.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Jeong0806, All Rights Reserved.
+// Author Jeong Seong In (0806jsi@gmail.com)
+
+#ifndef JEONG0806_MAHT_UTILS_HPP_
+#define JEONG0806_MAHT_UTILS_HPP_
+
+namespace jeong0806::math {
+//사칙연산
+
+//비교연산
+}  // namespace jeong0806::math
+
+#endif
---
+++ src/utils.cpp
@@ -0,0 +1,2 @@
+// Copyright (c) 2023 Jeong0806, All Rights Reserved.
+// Author Jeong Seong In (0806jsi@gmail.com)
