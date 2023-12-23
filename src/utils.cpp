e8018a1af1570b9c4e38ac0da81355ab2b1416fc
Jeong0806 <Jeong0806@github.com>
2023-12-24 00:44:08 +0900

    Made Add function implementation

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,3 +2,16 @@
 // Author Jeong Seong In (0806jsi@gmail.com)
 
 #include "math/utils.hpp"
+
+#include <cmath>
+#include <stdexcept>
+
+namespace jeong0806::math {
+auto Add(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs)) {
+    throw std::invalid_argument("");
+  }
+  return lhs + rhs;
+}
+}  // namespace jeong0806::math
